018d40421ca1f7d5b0b0e1efdcf8d04fed54053a
zdravkostanin1 <zdravko.stanin@gmail.com>
2022-12-26 18:15:37 +0200

    created a new challenges_page, and continue implementation of BottomNavBar

---
+++ lib/challenges_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ChallengesPage extends StatefulWidget {
+  const ChallengesPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChallengesPage> createState() => _ChallengesPageState();
+}
+
+class _ChallengesPageState extends State<ChallengesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'challenges_page.dart';
 import 'sign_up.dart';
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,62 +18,85 @@ void signUserOut() async {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _page = 0;
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+
+  void switchNavBar(int screen) {
+    switch(screen) {
+      case 2:
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const ChallengesPage()));
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home Page',
       home: Scaffold(
-        bottomNavigationBar: SnakeNavigationBar.color(
-          behaviour: SnakeBarBehaviour.floating,
-          snakeShape: SnakeShape.rectangle,
-          snakeViewColor: Colors.black,
-          // shape: ShapeBorder(),
-          unselectedItemColor: Colors.black,
-          onTap: (int i) {},
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'settings'),
-            BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: 'calendar'),
-            BottomNavigationBarItem(icon: Icon(Icons.track_changes), label: 'challenges'),
-          ],
+        bottomNavigationBar: Builder(
+          builder: (context) {
+            return CurvedNavigationBar(
+              key: _bottomNavigationKey,
+              backgroundColor: Colors.black,
+              items: const <Widget>[
+                Icon(Icons.person, size: 35),
+                Icon(Icons.calendar_month, size: 35),
+                Icon(Icons.track_changes, size: 35),
+                Icon(Icons.settings, size: 35),
+              ],
+              onTap: (index) {
+                // if index x is clicked - then do X
+                setState(() {
+                  switch(index) {
+                    case 2:
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ChallengesPage()));
+                      break;
+                  }
+                  // switchNavBar(index);
+                });
+              },
+            );
+          }
         ),
+        // remove line below if needed.
         resizeToAvoidBottomInset: false,
         backgroundColor: const Color(0xff0B0A0A),
         body: Builder(builder: (BuildContext context) {
           return SafeArea(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TextButton(
-                  onPressed: () {
-                    signUserOut();
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUp()));
-                  },
-                  child: const Text(
-                    'Sign OUT',
-                    style: TextStyle(
-                      fontSize: 30.0,
-                    ),
-                  ),
-                ),
-                // SafeArea(
-                //   child: CustomNavigationBar(
-                //     onTap: (int s) {},
-                //     items: [
-                //       CustomNavigationBarItem(
-                //         icon: const Icon(Icons.settings),
-                //       ),
-                //       CustomNavigationBarItem(
-                //         icon: const Icon(Icons.calendar_month),
-                //       ),
-                //       CustomNavigationBarItem(
-                //         icon: const Icon(Icons.add_chart_sharp),
-                //       ),
-                //     ],
+                // Text(_page.toString(), textScaleFactor: 10.0),
+                // ElevatedButton(
+                //   child: Text('Go To Page of index 1'),
+                //   onPressed: () {
+                //     //Page change using state does the same as clicking index 1 navigation button
+                //     final CurvedNavigationBarState? navBarState =
+                //         _bottomNavigationKey.currentState;
+                //     navBarState?.setPage(1);
+                //   },
+                // ),
+                // TextButton(
+                //   onPressed: () {
+                //     signUserOut();
+                //     Navigator.push(
+                //         context,
+                //         MaterialPageRoute(
+                //             builder: (context) => const SignUp()));
+                //   },
+                //   child: const Text(
+                //     'Sign OUT',
+                //     style: TextStyle(
+                //       fontSize: 30.0,
+                //     ),
                 //   ),
                 // ),
               ],
