dc6045815ec6aad3285fdffea53b6c7390fc2e8e
zdravkostanin1 <zdravko.stanin@gmail.com>
2022-12-29 09:09:52 +0200

    trying to Implement Beamer

--- lib/challenges_page.dart
+++ lib/challenges_page.dart
@@ -11,6 +11,16 @@ class ChallengesPage extends StatefulWidget {
 class _ChallengesPageState extends State<ChallengesPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Column(
+        children: const [
+          Scaffold(
+            body: Center(
+              child: Text('ADDDD'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'challenges_page.dart';
@@ -7,7 +8,19 @@ import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  // removed const keyword
+  //const
+  HomePage({Key? key}) : super(key: key);
+
+  final routerDelegate = BeamerDelegate(
+    initialPath: '/home_page',
+    locationBuilder: RoutesLocationBuilder(
+      routes: {
+        '*': (context, state, data) => const Router(),
+      },
+    ),
+  );
+
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -18,18 +31,18 @@ void signUserOut() async {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _page = 0;
-  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+  // int _page = 0;
+  // final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   void switchNavBar(int screen) {
-    switch(screen) {
-      case 2:
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => const ChallengesPage()));
-        break;
-    }
+    // switch(screen) {
+    //   case 2:
+    //     Navigator.push(
+    //         context,
+    //         MaterialPageRoute(
+    //             builder: (context) => const ChallengesPage()));
+    //     break;
+    // }
   }
 
   @override
@@ -40,7 +53,7 @@ class _HomePageState extends State<HomePage> {
         bottomNavigationBar: Builder(
           builder: (context) {
             return CurvedNavigationBar(
-              key: _bottomNavigationKey,
+              // key: _bottomNavigationKey,
               backgroundColor: Colors.black,
               items: const <Widget>[
                 Icon(Icons.person, size: 35),
@@ -49,18 +62,19 @@ class _HomePageState extends State<HomePage> {
                 Icon(Icons.settings, size: 35),
               ],
               onTap: (index) {
+                // TODO: Implement different pages with navigation bar
                 // if index x is clicked - then do X
                 setState(() {
-                  switch(index) {
-                    case 2:
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ChallengesPage()));
-                      break;
+                  // switch(index) {
+                  //   case 2:
+                  //     Navigator.push(
+                  //         context,
+                  //         MaterialPageRoute(
+                  //             builder: (context) => const ChallengesPage()));
+                  //     break;
                   }
                   // switchNavBar(index);
-                });
+                );
               },
             );
           }
@@ -107,3 +121,132 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+class ALocation extends BeamLocation<BeamState> {
+  ALocation(super.routeInformation);
+  @override
+  List<String> get pathPatterns => ['/*'];
+
+  @override
+  List<BeamPage> buildPages(BuildContext context, BeamState state) => [
+     BeamPage(
+      key: const ValueKey('home_page'),
+      title: 'Tab A',
+      type: BeamPageType.noTransition,
+      child: HomePage(),
+    ),
+    // if (state.uri.pathSegments.length == 2)
+    //   const BeamPage(
+    //     key: ValueKey('a/details'),
+    //     title: 'Details A',
+    //     child: DetailsScreen(label: 'A'),
+    //   ),
+  ];
+}
+
+class BLocation extends BeamLocation<BeamState> {
+  BLocation(super.routeInformation);
+  @override
+  List<String> get pathPatterns => ['/*'];
+
+  @override
+  List<BeamPage> buildPages(BuildContext context, BeamState state) => [
+    const BeamPage(
+      key: ValueKey('challenges_page'),
+      title: 'Tab A',
+      type: BeamPageType.noTransition,
+      child: ChallengesPage(),
+    ),
+    // if (state.uri.pathSegments.length == 2)
+    //   const BeamPage(
+    //     key: ValueKey('a/details'),
+    //     title: 'Details A',
+    //     child: DetailsScreen(label: 'A'),
+    //   ),
+  ];
+}
+
+
+class Router extends StatefulWidget {
+  const Router({Key? key}) : super(key: key);
+
+  @override
+  State<Router> createState() => _RouterState();
+}
+
+class _RouterState extends State<Router> {
+  // keep track of the currently selected index
+  late int _currentIndex;
+
+  // create two nested delegates
+  final _routerDelegates = [
+    BeamerDelegate(
+      initialPath: '/home_page',
+      locationBuilder: (routeInformation, _) {
+        if (routeInformation.location!.contains('/home_page')) {
+          return ALocation(routeInformation);
+        }
+        return NotFound(path: routeInformation.location!);
+      },
+    ),
+    BeamerDelegate(
+      initialPath: '/challenges_page',
+      locationBuilder: (routeInformation, _) {
+        if (routeInformation.location!.contains('/challenges_page')) {
+          return BLocation(routeInformation);
+        }
+        return NotFound(path: routeInformation.location!);
+      },
+    ),
+  ];
+
+  // update the _currentIndex if necessary
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final uriString = Beamer.of(context).configuration.location!;
+    _currentIndex = uriString.contains('/home_page') ? 0 : 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          // use Beamer widgets as children
+          Beamer(
+            routerDelegate: _routerDelegates[0],
+          ),
+          Beamer(
+            routerDelegate: _routerDelegates[1],
+          ),
+        ],
+      ),
+      bottomNavigationBar: Builder(
+          builder: (context) {
+            return CurvedNavigationBar(
+              index: _currentIndex,
+              // key: _bottomNavigationKey,
+              backgroundColor: Colors.black,
+              items: const <Widget>[
+                Icon(Icons.person, size: 35),
+                Icon(Icons.calendar_month, size: 35),
+                Icon(Icons.track_changes, size: 35),
+                Icon(Icons.settings, size: 35),
+              ],
+              onTap: (index) {
+                // TODO: Implement different pages with navigation bar
+                // if index x is clicked - then do X
+                if (index != _currentIndex) {
+                  setState(() => _currentIndex = index);
+                  _routerDelegates[_currentIndex].update(rebuild: false);
+                }
+              },
+            );
+          }
+      ),
+    );
+  }
+}
+
