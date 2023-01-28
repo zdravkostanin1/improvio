34cc0fd5b4b32f6b752dd280a0eb1a247d67cc2a
zdravkostanin1 <zdravko.stanin@gmail.com>
2023-01-28 09:08:00 +0200

    added Goals text and short-term text to goals page

--- lib/challenges_page.dart
+++ lib/challenges_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ChallengesPage extends StatefulWidget {
   const ChallengesPage({Key? key}) : super(key: key);
 
@@ -11,13 +10,37 @@ class ChallengesPage extends StatefulWidget {
 class _ChallengesPageState extends State<ChallengesPage> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Center(child: Text(
-          'Challenges', style: TextStyle(color: Colors.white),
-        ),),
-      )
+        body: Column(
+          children: const [
+            Padding(
+              padding: EdgeInsets.only(left: 25.0, top: 30.0),
+              child: Text(
+                'GOALS',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 55.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 1.0, top: 30.0),
+              child: Text(
+                'SHORT-TERM:',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
