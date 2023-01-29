a4349afbd28e031ac2fe2d01b1a44c22cc021a85
zdravkostanin1 <zdravko.stanin@gmail.com>
2023-01-29 08:58:40 +0200

    started building the prototype of Goals Page UI, added text widgets, and created a method which retrieves the current month that we're in

--- lib/challenges_page.dart
+++ lib/challenges_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ChallengesPage extends StatefulWidget {
   const ChallengesPage({Key? key}) : super(key: key);
@@ -7,16 +8,34 @@ class ChallengesPage extends StatefulWidget {
   State<ChallengesPage> createState() => _ChallengesPageState();
 }
 
+
+// TO GET THE CURRENT MONTH:
+String getMonth() {
+  final dateObject = DateTime.now();
+  // GET THE CURRENT MONTH:
+  return DateFormat.MMMM().format(dateObject);
+}
+
+
 class _ChallengesPageState extends State<ChallengesPage> {
+  String month = getMonth();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getMonth();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Column(
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(left: 25.0, top: 30.0),
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0, top: 50.0),
               child: Text(
                 'GOALS',
                 style: TextStyle(
@@ -25,14 +44,21 @@ class _ChallengesPageState extends State<ChallengesPage> {
                     fontWeight: FontWeight.bold),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 1.0, top: 30.0),
+            const Padding(
+              padding: EdgeInsets.only(left: 1.0, top: 10.0, right: 19.0),
               child: Text(
                 'SHORT-TERM:',
                 style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 13.5, top: 10.0, right: 0.0),
+              child: Text(
+                'Goals for: $month',
+                style: const TextStyle(
                     color: Colors.white,
                     fontSize: 20.0,
                     fontWeight: FontWeight.bold),
